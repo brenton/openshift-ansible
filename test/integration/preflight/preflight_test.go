3648d587b1c9538d50af1a631085f461095f09c0
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-11-04 19:07:35 +0100

    Update placeholder tests
    Now giving a better example of what we want to achieve.

--- test/integration/preflight/preflight_test.go
+++ test/integration/preflight/preflight_test.go
@@ -3,31 +3,59 @@ package preflight
 import (
 	"bytes"
 	"os/exec"
+	"syscall"
 	"testing"
 )
 
-func TestPing1(t *testing.T) {
+type PlaybookTest struct {
+	// inputs
+	Path string
+	// expected outputs
+	ExitCode int
+	Output   []string // zero or more strings that should be in the output
+}
+
+func (p PlaybookTest) Run(t *testing.T) {
 	t.Parallel()
-	cmd := exec.Command("ansible-playbook", "test_ping1.yml")
+	cmd := exec.Command("ansible-playbook", p.Path)
 	b, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Errorf("playbook execution failed: %v", err)
+	if p.ExitCode == 0 {
+		if err != nil {
+			t.Errorf("playbook execution failed: %v", err)
+		}
+	} else {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if gotCode := exitErr.Sys().(syscall.WaitStatus).ExitStatus(); gotCode != p.ExitCode {
+				t.Errorf("got exit code %v, want %v", gotCode, p.ExitCode)
+			}
+		} else {
+			t.Errorf("unexpected error: %#v", err)
+		}
 	}
-	want := "test ping 1"
-	if !bytes.Contains(b, []byte(want)) {
-		t.Errorf("got:\n%s\nwant to contain %q", b, want)
+	for _, s := range p.Output {
+		if !bytes.Contains(b, []byte(s)) {
+			t.Errorf("got:\n%s\nwant to contain %q", b, s)
+		}
 	}
 }
 
-func TestPing2(t *testing.T) {
-	t.Parallel()
-	cmd := exec.Command("ansible-playbook", "test_ping2.yml")
-	b, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Errorf("playbook execution failed: %v", err)
-	}
-	want := "test ping 2"
-	if !bytes.Contains(b, []byte(want)) {
-		t.Errorf("got:\n%s\nwant to contain %q", b, want)
-	}
+// note: TestPing and TestFail below are just placeholders. The idea is to
+// replace them with tests that call more intesting playbooks. However, the
+// initial structure of the tests may be just like that: run a command, capture
+// output, check for errors. I believe in most cases we will expect a non-nil
+// error from running the command (non-zero exit code).
+
+func TestPing(t *testing.T) {
+	PlaybookTest{
+		Path:   "test_ping.yml",
+		Output: []string{"[test ping]"},
+	}.Run(t)
+}
+
+func TestFail(t *testing.T) {
+	PlaybookTest{
+		Path:     "test_fail.yml",
+		ExitCode: 1,
+		Output:   []string{"[test fail]", `"msg": "Failed as requested from task"`},
+	}.Run(t)
 }
